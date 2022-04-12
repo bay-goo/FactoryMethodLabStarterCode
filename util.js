c4295c29164a6dc5c4a92cab03e77b7fc77baf53
bg-nrtoday <103073106+bg-nrtoday@users.noreply.github.com>
2022-04-12 14:50:36 -0700

    Well, maybe it's not the antivirus...

--- util.js
+++ util.js
@@ -1,17 +1,13 @@
-// 
+// Global constants. 
 //const url = "https://waterservices.usgs.gov/nwis/iv/?format=json,1.1&sites=14319500&parameterCd=00010&siteStatus=active"
-const url = "https://catfact.ninja/docs/api-docs.json/fact";
+const url = "https://catfact.ninja/fact";
 const temperature_data = -199;
 
-let fetchTemp = async function fetchTemp() {
-  let response = await fetch(url , {
-    mode: 'no-cors',
-    headers: {
-      'Access-Control-Allow-Origin':'*'
-    }
-  })
+let checkTemp = async function checkTemp() {
+  // fetch() returns a Promise, use then() and catch() methods
+  // to handle it.
+  let response = await fetch(url);
   let data = await response.json();
-  return data;
+  console.log(data.fact);
+  //alert(data.fact);
 }
-
-fetchTemp().then(data => console.log(data));
