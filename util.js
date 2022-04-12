7e07afeab85fd6a153cfffce161986e50cead22d
bg-nrtoday <103073106+bg-nrtoday@users.noreply.github.com>
2022-04-12 16:05:29 -0700

    Closer... At least I found the Thing.

--- util.js
+++ util.js
@@ -1,13 +1,10 @@
 // Global constants. 
 //const url = "https://waterservices.usgs.gov/nwis/iv/?format=json,1.1&sites=14319500&parameterCd=00010&siteStatus=active"
-const url = "https://catfact.ninja/fact";
+const url = "https://labs.waterdata.usgs.gov/sta/v1.1/Things('USGS-14319500')";
 const temperature_data = -199;
 
 let checkTemp = async function checkTemp() {
-  // fetch() returns a Promise, use then() and catch() methods
-  // to handle it.
   let response = await fetch(url);
   let data = await response.json();
-  console.log(data.fact);
-  //alert(data.fact);
+  console.log(data);
 }
