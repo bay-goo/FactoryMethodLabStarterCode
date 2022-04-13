1eb59bb5ac5d43f0eb26823be160f3f69ee7f382
bg-nrtoday <103073106+bg-nrtoday@users.noreply.github.com>
2022-04-13 11:16:05 -0700

    Temperature updates, working on stability.

--- util.js
+++ util.js
@@ -1,10 +1,23 @@
 // Global constants. 
-//const url = "https://waterservices.usgs.gov/nwis/iv/?format=json,1.1&sites=14319500&parameterCd=00010&siteStatus=active"
-const url = "https://labs.waterdata.usgs.gov/sta/v1.1/Things('USGS-14319500')";
-const temperature_data = -199;
+const url = "https://labs.waterdata.usgs.gov/sta/v1.1/Datastreams('6cbb1ab905a94794a3f4f8030004ee30')/Observations?$top=1";
+
+// Thank you stack overflow.
+function precisionRoundMod(number, precision) {
+  var factor = Math.pow(10, precision);
+  var n = precision < 0 ? number : 0.01 / factor + number;
+  return Math.round( n * factor) / factor;
+}
 
 let checkTemp = async function checkTemp() {
-  let response = await fetch(url);
-  let data = await response.json();
-  console.log(data);
+  fetch(url)
+  .then(response => response.json())
+  .then(data => {
+    var obj = JSON.parse(JSON.stringify(data));
+    var tempC = obj.value[0].result;
+    var tempF = precisionRoundMod((tempC * (1.8)) + 32, 2);
+    document.getElementById("temp").innerHTML = "North Umpqua River Temp: " + tempF + " F!"
+    console.log("MSG: Successful update.")
+  });
 }
+
+setInterval(checkTemp(), 5000);
